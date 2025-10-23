1cd0ad0eb6a229de2fe0452a8aab4add8b422f07
unknown <md.fazlerabbirayhan786@gmail.com>
2025-10-23 16:48:09 +0600

    Initial commit

--- src/components/PopularServices.jsx
+++ src/components/PopularServices.jsx
@@ -1,11 +1,32 @@
+import { useContext } from "react";
 import { FaStethoscope } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import servicesData from "../data/services.json";
+import { AuthContext } from "../provider/AuthProvider";
 
 const PopularServices = () => {
   const popularServices = servicesData
     .filter((service) => service.rating >= 4.5)
     .slice(0, 6);
+
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleServiceClick = (id) => {
+    if (!user) {
+      navigate("/login");
+    } else {
+      navigate(`/service/${id}`);
+    }
+  };
+
+  const handleAllServicesClick = () => {
+    if (!user) {
+      navigate("/login");
+    } else {
+      navigate("/services");
+    }
+  };
 
   return (
     <section className="container mx-auto px-4 mt-20 mb-16 text-center">
@@ -19,14 +40,17 @@ const PopularServices = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-10">
         {popularServices.map((service) => (
-          <div className="border border-gray-200 rounded shadow hover:shadow-lg hover:translate-y-[-5px] transition p-4 flex flex-col gap-2">
+          <div
+            key={service.serviceId}
+            className="border border-gray-200 rounded shadow hover:shadow-lg hover:translate-y-[-5px] transition p-4 flex flex-col gap-2"
+          >
             <img
               src={service.image}
               alt={service.serviceName}
               className="h-80 w-full object-cover rounded"
             />
             <div className="flex items-center gap-5">
-              <FaStethoscope className="w-10 h-10 text-[#ff8f8f]"></FaStethoscope>
+              <FaStethoscope className="w-10 h-10 text-[#ff8f8f]" />
               <div className="text-left">
                 <h3 className="font-bold text-lg mt-2 text-[#333]">
                   {service.serviceName}
@@ -35,22 +59,22 @@ const PopularServices = () => {
               </div>
             </div>
 
-            <Link
-              to={`/service/${service.serviceId}`}
+            <button
+              onClick={() => handleServiceClick(service.serviceId)}
               className="btn btn-error btn-outline hover:text-white w-40 mt-5"
             >
               View Details
-            </Link>
+            </button>
           </div>
         ))}
       </div>
 
-      <Link
-        to="/services"
+      <button
+        onClick={handleAllServicesClick}
         className="inline-block bg-[#FF8F8F] hover:bg-[#ff6b6b] text-white font-semibold px-8 py-3 rounded-full transition"
       >
         View All Services
-      </Link>
+      </button>
     </section>
   );
 };
